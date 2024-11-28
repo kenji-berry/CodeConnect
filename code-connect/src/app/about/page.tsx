1d743e21e66b93917a02834da669c76e77329ca2
kenji-berry <kenji.k.berry@gmail.com>
2024-11-28 21:46:14 +0000

    Add About page with CodeConnectTitle and NavBar components

---
+++ code-connect/src/app/about/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import CodeConnectTitle from '../Components/CodeConnectTitle'
+import NavBar from '../Components/NavBar'
+
+const page = () => {
+  return (
+    <div className="w-screen h-screen radial-background flex flex-col items-center">
+      <NavBar />
+      <CodeConnectTitle />
+      <div className='w-3/4'>
+        <h1 className='text-3xl'>About Code Connect</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat earum repellendus perspiciatis quam rem temporibus provident fuga ab rerum! Deleniti delectus nemo asperiores voluptate hic inventore eaque itaque eveniet facere! Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat earum repellendus perspiciatis quam rem temporibus provident fuga ab rerum! Deleniti delectus nemo asperiores voluptate hic inventore eaque itaque eveniet facere!Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat earum repellendus perspiciatis quam rem temporibus provident fuga ab rerum! Deleniti delectus nemo asperiores voluptate hic inventore eaque itaque eveniet facere!Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat earum repellendus perspiciatis quam rem temporibus provident fuga ab rerum! Deleniti delectus nemo asperiores voluptate hic inventore eaque itaque eveniet facere!Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat earum repellendus perspiciatis quam rem temporibus provident fuga ab rerum! Deleniti delectus nemo asperiores voluptate hic inventore eaque itaque eveniet facere!Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat earum repellendus perspiciatis quam rem temporibus provident fuga ab rerum! Deleniti delectus nemo asperiores voluptate hic inventore eaque itaque eveniet facere!Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat earum repellendus perspiciatis quam rem temporibus provident fuga ab rerum! Deleniti delectus nemo asperiores voluptate hic inventore eaque itaque eveniet facere!Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat earum repellendus perspiciatis quam rem temporibus provident fuga ab rerum! Deleniti delectus nemo asperiores voluptate hic inventore eaque itaque eveniet facere!Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat earum repellendus perspiciatis quam rem temporibus provident fuga ab rerum! Deleniti delectus nemo asperiores voluptate hic inventore eaque itaque eveniet facere!Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat earum repellendus perspiciatis quam rem temporibus provident fuga ab rerum! Deleniti delectus nemo asperiores voluptate hic inventore eaque itaque eveniet facere!</p>
+      </div>
+
+      <footer className="row-start-3 flex gap-6 flex-wrap justify-center"></footer>
+    </div>
+  )
+}
+
+export default page
