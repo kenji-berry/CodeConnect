f57d010c9df8711fb3af725a89954c57e72e5d3a
kenji-berry <kenji.k.berry@gmail.com>
2024-11-29 15:31:01 +0000

    Add Tag functionality

--- code-connect/src/app/post-project/page.tsx
+++ code-connect/src/app/post-project/page.tsx
@@ -1,8 +1,17 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import NavBar from "../Components/NavBar";
 import "./style.css";
+import MultiSelector from "../Components/MultiSelector";
 
-const page = () => {
+const Page = () => {
+  const tags = ["tag1", "tag2", "tag3", "tag4", "tag5"];
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+
+  const handleTagsChange = (tags: string[]) => {
+    setSelectedTags(tags);
+  };
+
   return (
     <div className="w-screen h-screen radial-background flex flex-col items-center">
       <NavBar />
@@ -14,11 +23,17 @@ const page = () => {
         <div className="bento-box half-width radial-background">
           Tech Stack:
         </div>
-        <div className="bento-box half-width radial-background">Tags:</div>
+        <div className="bento-box half-width radial-background">
+          <h4>Tags:</h4>
+          <MultiSelector
+            availableTags={tags}
+            onTagsChange={handleTagsChange}
+            initialTags={selectedTags}
+          />
+        </div>
         <div className="full-width">
           <h2>READ ONLY Project Information (as of xx/xx/xx)</h2>
         </div>
-
         <div className="bento-box half-width radial-background">Owner:</div>
         <div className="bento-box half-width radial-background">
           Issues Open:
@@ -31,4 +46,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
