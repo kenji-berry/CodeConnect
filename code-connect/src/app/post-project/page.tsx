c59b9d09206df379225ca7174f83d8f82c054996
kenji-berry <kenji.k.berry@gmail.com>
2024-11-29 15:24:51 +0000

    Update PostProject page layout and styles; enhance project information display

--- code-connect/src/app/post-project/page.tsx
+++ code-connect/src/app/post-project/page.tsx
@@ -1,22 +1,34 @@
-import React from 'react'
-import NavBar from '../Components/NavBar'
-import "./style.css"
+import React from "react";
+import NavBar from "../Components/NavBar";
+import "./style.css";
 
 const page = () => {
   return (
-    <div className='w-screen h-screen radial-background flex flex-col items-center'>
+    <div className="w-screen h-screen radial-background flex flex-col items-center">
       <NavBar />
-      <h1>Project Name</h1>
-      <div className='bento-container w-full'>
-        <div className='bento-box full-width radial-background'>Full Width Box 1</div>
-        <div className='bento-box half-width radial-background'>Half Width Box 1</div>
-        <div className='bento-box half-width radial-background'>Half Width Box 2</div>
-        <div className='bento-box half-width radial-background'>Half Width Box 3</div>
-        <div className='bento-box half-width radial-background'>Half Width Box 4</div>
-        <div className='bento-box full-width radial-background'>Full Width Box 2</div>
+      <h1 className="project-page-project-name">Project Name</h1>
+      <div className="bento-container w-full inria-sans-regular">
+        <div className="bento-box full-width radial-background">
+          Write your own project description or use existing README?
+        </div>
+        <div className="bento-box half-width radial-background">
+          Tech Stack:
+        </div>
+        <div className="bento-box half-width radial-background">Tags:</div>
+        <div className="full-width">
+          <h2>READ ONLY Project Information (as of xx/xx/xx)</h2>
+        </div>
+
+        <div className="bento-box half-width radial-background">Owner:</div>
+        <div className="bento-box half-width radial-background">
+          Issues Open:
+        </div>
+        <div className="bento-box full-width radial-background">
+          Recent Activity:
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
