8e172ca1b5a947db3e0e46d4106b1fdfe190b580
Kenji Berry <120066663+kenji-berry@users.noreply.github.com>
2025-01-25 16:35:41 +0000

    add initial github supabase integration

--- code-connect/src/app/Components/LoginButton.tsx
+++ code-connect/src/app/Components/LoginButton.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
+"use client";
+
+import React from 'react';
+import { supabase } from '@/supabaseClient';
 
 const LoginButton = () => {
+  const handleLogin = async () => {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: 'github',
+    });
+    if (error) console.error('Error logging in with GitHub:', error.message);
+  };
+
   return (
     <button 
+      onClick={handleLogin}
       className="bg-[--muted-red] hover: px-3 py-2 rounded-full transition-colors duration-200 inria-sans-bold text-off-white text-sm"
     >
       Log in with GitHub
     </button>
-  )
-}
+  );
+};
 
-export default LoginButton
+export default LoginButton;
