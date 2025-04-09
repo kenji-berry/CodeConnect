439223c570045f67d6baa8f91ae18290eb7e504d
Kenji Berry <120066663+kenji-berry@users.noreply.github.com>
2025-04-09 15:11:14 +0100

    set up jest roughtly

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+const nextJest = require('next/jest')
+
+// Providing the path to your Next.js app which will enable loading next.config.js and .env files
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+// Any custom config you want to pass to Jest
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.css$': 'identity-obj-proxy',
+  },
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!**/node_modules/**',
+  ],
+}
+
+module.exports = createJestConfig(customJestConfig)
