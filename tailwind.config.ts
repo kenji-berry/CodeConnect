c21af5b9b3d01878f6566f7e919fceff48b33b4a
kenji-berry <kenji.k.berry@gmail.com>
2025-04-10 04:30:31 +0100

    to string

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
       },
       keyframes: {
         slideDown: {
-          '0%': { transform: 'translateY(-20px)', opacity: 0 },
-          '100%': { transform: 'translateY(0)', opacity: 1 }
+          '0%': { transform: 'translateY(-20px)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: "1" }
         }
       }
     },
